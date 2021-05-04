b8aa4a36d11c53e12a8dc25fd51c09f86b8ccc21
prdeck <punidramesh@gmail.com>
2021-05-04 19:10:01 +0530

    minor rearrangements

--- auth/auth.py
+++ auth/auth.py
@@ -18,9 +18,13 @@ REDIRECT_URL = os.getenv('REDIRECT_URL')
 clientID = os.getenv('CLIENTID')
 secret = os.getenv('CLIENT_SECRET')
 STATE = os.getenv('STATE')
-PORT = os.getenv('PORT')
 access_token = ""
 refresh_token = ""
+
+def saveInfo(access_token, refresh_token):
+	subprocess.call("echo TIME=" + str(time.time()) + " >> .env",shell=True)
+	subprocess.call("echo ACCESS_TOKEN=" + access_token + " >> .env",shell=True)
+	subprocess.call("echo REFRESH_TOKEN=" + refresh_token + " >> .env",shell=True)
 
 def renewAccessToken():
 	REFRESH_TOKEN = os.getenv('REFRESH_TOKEN')
@@ -28,11 +32,16 @@ def renewAccessToken():
 	at = requests.post(REFRESH_URI).json()
 	access_token = at['access_token']
 	refresh_token = at['refresh_token']
-	subprocess.call("echo TIME=" + str(time.time()) + " >> .env",shell=True)
-	subprocess.call("echo PID=" + str(os.getpid()) + " >> .env",shell=True)
-	subprocess.call("echo ACCESS_TOKEN=" + access_token + " >> .env",shell=True)
-	subprocess.call("echo REFRESH_TOKEN=" + refresh_token + " >> .env",shell=True)
+	saveInfo()
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
 
 #default route
 @app.route('/')
@@ -43,19 +52,22 @@ def hello():
 @app.route('/auth/redirect')
 def getResponse():
 	code = request.args.get("code")
-	state = request.args.get("state")
-	if code != None and state == STATE:
-		ACCESS_URI = f"https://www.coinbase.com/oauth/token?grant_type=authorization_code&code={code}&client_id={clientID}&client_secret={secret}&redirect_uri={REDIRECT_URL}"
-		at = requests.post(ACCESS_URI).json()
-		access_token = at['access_token']
-		refresh_token = at['refresh_token']
-		subprocess.call("echo TIME=" + str(time.time()) + " >> .env",shell=True)
-		subprocess.call("echo PID=" + str(os.getpid()) + " >> .env",shell=True)
-		subprocess.call("echo ACCESS_TOKEN=" + access_token + " >> .env",shell=True)
-		subprocess.call("echo REFRESH_TOKEN=" + refresh_token + " >> .env",shell=True)
-		return render_template('complete.html')
-	else:
+	try:
+		if code != None:
+			ACCESS_URI = f"https://www.coinbase.com/oauth/token?grant_type=authorization_code&code={code}&client_id={clientID}&client_secret={secret}&redirect_uri={REDIRECT_URL}"
+			at = requests.post(ACCESS_URI).json()
+			access_token = at['access_token']
+			refresh_token = at['refresh_token']
+
+			if access_token == '' or refresh_token == '':
+				return render_template('error.html')
+
+			saveInfo(access_token, refresh_token)
+			return render_template('complete.html')
+		else:
 			return render_template('error.html')
+	finally:
+		sys.exit(0)
 
 if __name__ == '__main__':
-	app.run(port=PORT)
+	app.run(port=6660)
--- cli.py
+++ cli.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
-import os, subprocess, pathlib, time
+import os, subprocess, pathlib, time, multiprocessing
 from webbrowser import open_new
-from app.auth import renewAccessToken
+from auth.auth import renewAccessToken
 
 load_dotenv()
 login_status = os.getenv('LOGIN_STATUS')
@@ -9,25 +9,20 @@ REDIRECT_URL = os.getenv('REDIRECT_URL')
 clientID = os.getenv('CLIENTID')
 secret = os.getenv('CLIENT_SECRET')
 state = os.getenv('STATE')
-PORT = os.getenv('PORT')
-BASE_URL = os.getenv('BASE_URL')
+TERMINATE = os.getenv('TERMINATE')
 
-def auth():
+def renew():
+    if time.time() - float(os.getenv('TIME')) > 7200:
+        renewAccessToken()
+
+def login():
     path = pathlib.Path().absolute()
     #start the flask server for OAuth
-    subprocess.call("python3 " +  os.path.join(path,"app/auth.py") +  "&", shell=True)
+    subprocess.call("python3 " +  os.path.join(path,"auth/auth.py") +  "&", shell=True)
     AUTH_URI = ('https://www.coinbase.com/oauth/' 
         + 'authorize?response_type=code&client_id=' + clientID + '&redirect_uri=' 
         + REDIRECT_URL + '&state=' + state
         + '&scope=wallet:user:read,wallet:accounts:read' + '&code=' + '302')
     open_new(AUTH_URI)
 
-if login_status == 'False':
-    auth()
-    print("login successful")
-
-def renewAccessToken():
-    curr_time = time.time()
-    refresh_time = float(os.getenv("TIME"))
-    if refresh_time - curr_time == 7200.0:
-        renewAccessToken()
+login()
